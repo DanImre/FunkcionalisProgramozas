835105b35eb3ce25c81e37b2c75a1b168a9b9110
Dán Imre <uuuhsk@INF.ELTE.HU>
2022-09-21 16:58:22 +0200

    kérésre

---
+++ Progalap/42 Vonatok Szemben/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class megallo
+        {
+            public int? megallePiripócs { get; set; }
+            public int? megalleKukutyin { get; set; }
+
+            public int idoFromLeft { get; set; }
+            public int idoFromRight { get; set; }
+
+            public megallo(int _idoFromLeft, int _idoFromRight, int? _megallePiripócs = null, int? _megalleKukutyin = null)
+            {
+                idoFromLeft = _idoFromLeft;
+                idoFromRight = _idoFromRight;
+                megallePiripócs = _megallePiripócs;
+                megalleKukutyin = _megalleKukutyin;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int K_ind = int.Parse(s[0]);
+            int P_ind = int.Parse(s[1]);
+            int n = int.Parse(s[2]);
+
+            s = Console.ReadLine().Split(' ');//idők
+            List<int> menetidoMegallokKozott = new List<int>();
+            foreach (var item in s)
+                menetidoMegallokKozott.Add(int.Parse(item));
+
+            string[] varakozasokKUKUSTRING = Console.ReadLine().Split(' ');//varakozás Kukutyinból
+            List<int> varakozasokKUKU = new List<int>();
+            foreach (var item in varakozasokKUKUSTRING)
+                varakozasokKUKU.Add(int.Parse(item));
+            string[] varakozasokPIRISTRING = Console.ReadLine().Split(' ');//varakozás Piripócs
+            List<int> varakozasokPIRI = new List<int>();
+            foreach (var item in varakozasokPIRISTRING)
+                varakozasokPIRI.Add(int.Parse(item));
+
+            int db = 0;
+            for (int i = 0; i < n; i++)
+                if (varakozasokKUKU[i] == 0 && varakozasokPIRI[n - i - 1] == 0)
+                    ++db;
+
+            //1. feladat
+            Console.WriteLine("#");
+            Console.WriteLine(db);
+
+            //2. feladat
+            Console.WriteLine("#");
+            int KUKUido = K_ind;
+            int PIPIido = P_ind;
+            foreach (var item in menetidoMegallokKozott)
+            {
+                KUKUido += item;
+                PIPIido += item;
+            }
+
+            foreach (var item in varakozasokKUKU)
+                KUKUido += item;
+
+            foreach (var item in varakozasokPIRI)
+                PIPIido += item;
+
+            Console.WriteLine(KUKUido + " " + PIPIido);
+
+            //3. feladat
+            Console.WriteLine("#");
+            List<megallo> x = new List<megallo>();
+            for (int i = 0; i < n; i++)
+                x.Add(new megallo(0, 0));
+
+            for (int i = 0; i < menetidoMegallokKozott.Count-1; i++)
+            {
+                x[i].idoFromLeft = menetidoMegallokKozott[i];
+                x[i].idoFromRight = menetidoMegallokKozott[i + 1];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (varakozasokKUKU[i] > 0)
+                    x[i].megalleKukutyin = varakozasokKUKU[i];
+                if (varakozasokPIRI[i] > 0)
+                    x[i].megallePiripócs = varakozasokPIRI[i];
+            }
+
+            x.Insert(0, new megallo(K_ind, menetidoMegallokKozott[0], -1, -1));
+            x.Insert(x.Count, new megallo(menetidoMegallokKozott[menetidoMegallokKozott.Count-1],P_ind, -1, -1));
+            //3. feladat
+            /*
+            int aktKUKU = K_ind;
+            int aktPIPI = P_ind;
+            (int where, int howMuch) varakozas = (0, 0);*/
+
+            (int megallo, int elteltido) aktKUKU = (0,0);
+            (int megallo, int elteltido) aktPIPI = (0,0);
+            int time = 0;
+            while (true)
+            {
+                //KUKU:
+                if(x[aktKUKU.megallo].idoFromLeft == 0)
+                {
+                    if(x[aktKUKU.megallo].megalleKukutyin.HasValue)
+                }
+            }
+            /*
+            for (int i = 0; i < x.Count-1; i++)
+            {
+                if(Math.Abs(i - (x.Count-1 -i)) == 1 //(i,aktKUKU,aktPIPI))
+                {
+                    //kevesebb e az odaút, mint a várakozási idő
+                    if (x[i].idoFromRight > x[x.Count - 1 - i].idoFromLeft) //aktPIPI indul előbb, neki van elsőbbség
+                    {
+                        varakozas = ((x.Count - 1 - i), );
+                    }
+                    else //aktKUKu elsőbbség
+                    {
+
+                    }
+                }
+                aktKUKU += x[i].idoFromLeft;
+                if (x[i].megalleKukutyin.HasValue)
+                    aktKUKU += x[i].megalleKukutyin.Value;
+
+                aktPIPI += x[x.Count - i - 1].idoFromRight;
+                if (x[x.Count - i - 1].megallePiripócs.HasValue)
+                    aktPIPI += x[x.Count - i - 1].megallePiripócs.Value;
+
+            }*/
+        }
+    }
+}
---
+++ Progalap/MasodikOra/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Resources;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(int, int)> x = new List<(int, int)>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                x.Add((int.Parse(s[0]), int.Parse(s[1])));
+            }
+            
+            //1
+            Console.WriteLine("#");
+            Console.WriteLine(x.Where(kk => kk.Item1 <= 0 || kk.Item2 <= 0).Count());
+            
+            //2
+            Console.WriteLine("#");
+            int max = 0;
+            int maxI = 0;
+            for (int i = 0; i < n; i++)
+                if(Math.Abs(x[i].Item1 - x[i].Item2) > max)
+                {
+                    max = Math.Abs(x[i].Item1 - x[i].Item2);
+                    maxI = i;
+                }
+
+            Console.WriteLine(maxI + 1);
+
+            //3.
+            Console.WriteLine("#");
+            bool van = false;
+            for (int i = 1; i < n; i++)
+                if(x[i-1].Item1 > x[i].Item2)
+                {
+                    Console.WriteLine(i + 1);
+                    van = true;
+                    break;
+                }
+            if(!van)
+                Console.WriteLine("-1");
+            //4.
+            Console.WriteLine("#");
+            Console.Write(x.Where(kk => kk.Item1 <= 0 && kk.Item2 > 0).Count());
+            for (int i = 0; i < n; i++)
+                if (x[i].Item1 <= 0 && x[i].Item2 > 0)
+                    Console.Write(" " + (i + 1));
+
+
+        }
+    }
+}
