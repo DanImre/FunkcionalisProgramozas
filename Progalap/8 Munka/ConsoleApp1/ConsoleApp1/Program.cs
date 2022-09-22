2ff32b26f0a8fe20e4bd7229edf440004162357a
Dán Imre <uuuhsk@INF.ELTE.HU>
2022-09-22 09:54:09 +0200

    2022.09.22 9:54

---
+++ Progalap/8 Munka/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class kozvetites
+        {
+            public int ceg { get; set; }
+            public int dolgozo { get; set; }
+            public int ev { get; set; }
+            public int nem { get; set; }
+
+            public kozvetites(int _ceg, int _dolgozo, int _ev, int _nem)
+            {
+                ceg = _ceg;
+                dolgozo = _dolgozo;
+                ev = _ev;
+                nem = _nem;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int c = int.Parse(s[0]);
+            int k = int.Parse(s[1]);
+
+            List<kozvetites> x = new List<kozvetites>();
+            for (int i = 0; i < k; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                x.Add(new kozvetites(int.Parse(s[0]), int.Parse(s[1]), int.Parse(s[2]), int.Parse(s[3])));
+            }
+
+            //1. feldat
+            Console.WriteLine(x.Count(kk => kk.nem == 0) + " " + x.Count(kk => kk.nem == 1));
+
+            //2. feladat
+            Dictionary<int, int> cegEladok = new Dictionary<int, int>();
+            foreach (var item in x)
+            {
+                if (!cegEladok.ContainsKey(item.ceg))
+                    cegEladok.Add(item.ceg, 1);
+                else
+                    ++cegEladok[item.ceg];
+            }
+
+            Console.WriteLine(cegEladok.Count);
+
+            //3. feladat
+            for (int i = 1; i < c+1; i++)
+                if(cegEladok.ContainsKey(i))
+                    Console.Write(cegEladok[i] + " ");
+                else
+                    Console.Write(0 + " ");
+
+            Console.WriteLine();
+            //4. feladat
+            foreach (var item in x.OrderBy(kk => kk.ev))
+                Console.Write(item.dolgozo + " ");
+
+        }
+    }
+}
---
+++ Progalap/9 Forgalom/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class jelentes
+        {
+            public int nap { get; set; }
+            public int uzlet { get; set; }
+            public int db { get; set; }
+
+            public jelentes(int _nap, int _uzlet, int _db)
+            {
+                nap = _nap;
+                uzlet = _uzlet;
+                db = _db;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int napok = int.Parse(s[0]); 
+            int uzletek = int.Parse(s[1]); 
+            int n = int.Parse(s[2]);
+
+            List<jelentes> x = new List<jelentes>();
+
+            for (int i = 0; i < n; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                x.Add(new jelentes(int.Parse(s[0]), int.Parse(s[1]), int.Parse(s[2])));
+            }
+
+            //1. feladat
+            Console.WriteLine(x.Max(kk => kk.db));
+
+            //2. feladat
+            Dictionary<int, int> napEladott = new Dictionary<int, int>();
+            foreach (var item in x)
+                if (!napEladott.ContainsKey(item.nap))
+                    napEladott.Add(item.nap, 1);
+                else
+                    ++napEladott[item.nap];
+
+            Console.WriteLine(napEladott.Count);
+
+            //3. feladat
+            List<int> uzletekAdas = new List<int>();
+            for (int i = 0; i < uzletek; i++)
+                uzletekAdas.Add(0);
+
+            foreach (var item in x)
+                ++uzletekAdas[item.uzlet-1];
+
+            int max = uzletekAdas[0];
+            int maxI = 0;
+            for (int i = 1; i < uzletekAdas.Count; i++)
+                if(max < uzletekAdas[i])
+                {
+                    max = uzletekAdas[i];
+                    maxI = i;
+                }
+
+            Console.WriteLine((maxI + 1) + " " + max);
+
+            //4. feladat
+            int? start = null;
+            int? end = null;
+            for (int i = 0; i < napok; i++)
+            {
+                int? tempStart = null;
+                if (napEladott.ContainsKey(i))
+                    tempStart = i;
+
+                while (napEladott.ContainsKey(i) && i < napok)
+                    ++i;
+
+                if(tempStart.HasValue)
+                    if(!start.HasValue)
+                    {
+                        start = tempStart;
+                        end = i;
+                    }
+                    else if (end - start < i - tempStart)
+                    {
+                        start = tempStart;
+                        end = i;
+                    }
+            }
+
+            Console.WriteLine(start + " " + (end-1));
+        }
+    }
+}
