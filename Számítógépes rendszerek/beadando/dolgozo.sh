97b98475311e689ce2318655b5f2195d78512962
DanImre <dan.imre.17f@gmail.com>
2022-10-27 11:20:15 +0200

    szamrend hazi kesz

--- Számítógépes rendszerek/beadando/dolgozo.sh
+++ Számítógépes rendszerek/beadando/dolgozo.sh
@@ -1,44 +1,81 @@
 #!/bin/bash
 filename='be.txt'
 n=1;
-declare -a nagyArray
+declare -a nevekArray
+declare -a besorolasokArray
+declare -a munkakorokArray # itt '-'-ekkel elvalasztva lesznek
 declare -a sorhosszak #literalisan a sorok hossza
 sorhosszak[0]=0
 while read line; 
 do
     IFS=',' read -r -a array <<< "$line" #beolvassa az array-be
+    munkakorokArray[n]=""
     for index in "${!array[@]}"
     do
         if [ $index -gt 0 ]
         then
             array[index]=${array[index]:1}
+        else
+            nevekArray[n]=${array[index]} 
         fi
-        actualIndex=$((index-((n-1))+sorhosszak[$((n-1))]))
-        nagyArray[actualIndex]=${array[index]} #leszedi a space-t és beteszi az arraybe
-        sorhosszak[n]=$((index+1))
-        #echo "${nagyArray[actualIndex]}"
+
+        if [ $index -eq 1 ]
+        then
+            besorolasokArray[n]=${array[index]}
+        fi
+
+        if [ $index -gt 1 ]
+        then
+            if [ $index -eq ${#array[@]} ]
+            then
+                munkakorokArray[n]="${munkakorokArray[n]}${array[index]}"
+            else    
+                munkakorokArray[n]="${munkakorokArray[n]}${array[index]}-"
+            fi
+        fi
     done
-    #arrayOfArrays[n]=$array
     n=$((n+1))
 done < $filename
 
 #1. feladat
-echo "Elso feladat:"
-declare -a pultosok
-n=1
-for index in "${!nagyArray[@]}"
+echo "a) feladat:"
+n=0
+for index in "${!besorolasokArray[@]}"
 do
-    actualIndex=$((index-((n-1))+sorhosszak[$((n-1))]))
-    temp="${nagyArray[actualIndex]}"
-    if [ $active -eq $"Pultos" ]
+    if [ "${besorolasokArray[index]}" == "Pultos" ]
     then
-        pultosok[n]=${nagyArray[((actualIndex-index))]}
+        echo "${nevekArray[index]}"
         n=$((n+1))
     fi
-    ##echo "$index ${array[index]}"
 done
 
-for index in "${!pultosok[@]}"
+if [ $n == "0" ]
+then
+    echo "NINCS"
+fi
+
+#2. feladat
+echo "b) feladat:"
+for index in "${!nevekArray[@]}"
 do
-    echo "${pultosok[index]}, "
+    IFS='-' read -r -a array <<< "${munkakorokArray[index]}"
+    if [ ${#array[@]} -gt 2 ]
+    then
+        echo "${nevekArray[index]}"
+    fi
 done
+
+#3. feladat
+echo "c) feladat:"
+echo -n "Adjon meg egy besorolást: "
+read besorolas
+n=0
+for index in "${!besorolasokArray[@]}"
+do
+    if [ "${besorolasokArray[index]}" == "$besorolas" ]
+    then
+        n=$((n+1))
+    fi
+done
+
+echo "$n"
