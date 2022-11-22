c8e40db6c34b85e03e4847dc8e8b1a2201a94398
Dán Imre <uuuhsk@INF.ELTE.HU>
2022-11-22 17:23:02 +0100

    4. funprog zh

---
+++ Imperatív programozás/tizedik.c
@@ -0,0 +1,13 @@
+#include <memory.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+    //majd talán Marci
+}
