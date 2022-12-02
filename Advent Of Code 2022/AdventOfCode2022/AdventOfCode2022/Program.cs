6141aa61de047c9027987a714cdc5104a8a9214e
DanImre <dan.imre.17f@gmail.com>
2022-12-02 16:39:22 +0100

    Advent Of Code 2022 Day 2 Complete

--- Advent Of Code 2022/AdventOfCode2022/AdventOfCode2022/Program.cs
+++ Advent Of Code 2022/AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -23,18 +23,76 @@ namespace AdventOfCode2022
                 if (s.Count != 0)
                     s.RemoveAt(0);
             }
-            Console.WriteLine(elfs.Max());
+            Console.WriteLine("Top calories: " + elfs.Max());
 
             //2. rész:
             elfs.Sort();
-            Console.WriteLine(elfs[elfs.Count - 1] + elfs[elfs.Count - 2] + elfs[elfs.Count - 3]);
+            Console.WriteLine("Sum of the top 3 calories: " + elfs[elfs.Count - 1] + elfs[elfs.Count - 2] + elfs[elfs.Count - 3]);
         }
+
+        public static void Masodik()
+        {
+            Dictionary<string, int> values = new Dictionary<string, int>();
+            values.Add("A", 1);
+            values.Add("B", 2);
+            values.Add("C", 3);
+            values.Add("X", 1);
+            values.Add("Y", 2);
+            values.Add("Z", 3);
+
+            int sum = 0;
+
+            string[] s = File.ReadAllLines("masodik.txt");
+            foreach (var item in s)
+            {
+                string[] temp = item.Split(' ');
+                int enemyValue = values[temp[0]];
+                int myValue = values[temp[1]];
+
+                sum += myValue;
+
+                if (myValue == enemyValue) //draw
+                    sum += 3;
+                else if (myValue - enemyValue == 1 || myValue - enemyValue == -2) //win
+                    sum += 6;
+            }
+
+            Console.WriteLine("Total score: " + sum);
+
+            //2. rész
+            sum = 0;
+            foreach (var item in s)
+            {
+                string[] temp = item.Split(' ');
+                int enemyValue = values[temp[0]];
+                int myValue = values[temp[1]];
+
+                if (myValue == 1) //Need to lose
+                    sum += (enemyValue - 1 == 0) ? 3 : enemyValue - 1;
+                else if (myValue == 2) //draw
+                    sum += 3 + enemyValue;
+                else //win
+                    sum += 6 + (enemyValue + 1 == 4 ? 1 : enemyValue + 1);
+            }
+            Console.WriteLine("Total score with the top secret strategy: " + sum);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Melyik feladat: ");
             int n = int.Parse(Console.ReadLine());
-            if (n == 1)
-                Elso();
+            switch (n)
+            {
+                case 1:
+                    Elso();
+                    break;
+                case 2:
+                    Masodik();
+                    break;
+                default:
+                    Console.WriteLine("Nincs ilyen feladat");
+                    break;
+            }
         }
     }
 }
