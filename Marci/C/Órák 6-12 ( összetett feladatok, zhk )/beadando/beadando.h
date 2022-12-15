f8f88911c002ed393e10d7e7d3b1c59ea2a48ff0
RVR826 <papp.marton20030929@gmail.com>
2022-12-15 11:35:39 +0100

    c beadando kesz

---
+++ Marci/C/Órák 6-12 ( összetett feladatok, zhk )/beadando/beadando.h
@@ -0,0 +1,7 @@
+#ifndef BEAD
+#define BEAD
+
+char **revall(char **, int);
+char **beolvas(char *, int, int *);
+
+#endif
--- Marci/C/Órák 6-12 ( összetett feladatok, zhk )/ropzh.c
+++ Marci/C/Órák 6-12 ( összetett feladatok, zhk )/ropzh.c
@@ -1,8 +1,9 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-int main() 
+int main()
 {
-    
     return 0;
 }
